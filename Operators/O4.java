e55d60bb4c3ba431d6dabd06d1e86e380d6869c9
DIVYA BHARATHI S <120720337+NSDivyaB@users.noreply.github.com>
2026-02-06 20:56:46 +0530

    Add files via upload

---
+++ Operators/O4.java
@@ -0,0 +1,13 @@
+package Operators_Program;
+public class O4{
+	    public static void main(String[] args){
+	        int a=5,b=10;
+	        int temp;
+	        temp=a;
+	        a=b;
+	        b=temp;
+	        System.out.println("a="+a +",b="+b);
+	    }
+	}
+
+
